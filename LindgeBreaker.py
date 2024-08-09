1e3ab84250153b62e6eb844c40d4be6475514f16
不曾想过 <Asjt2024@outlook.com>
2024-08-09 21:19:07 +0800

    改了一堆bug
    改不动了

--- LindgeBreaker.py
+++ LindgeBreaker.py
@@ -1,11 +1,3 @@
-'''
-凌极爆破者/LindgeBreaker SJT 版权所有
-本程序使用MPL2.0开源协议
-程序说明:
-    凌极爆破者/LindgeBreaker 是针对凌极机房控制系统受控端开发的爆破工具,可以让受控端HAPPY.
-    为了防止本程序大规模扩散,管理员可以紧急爆破客户端.
-    程序开发环境:Python 3.12.2
-'''
 import ttkbootstrap as ttk
 from ttkbootstrap.dialogs import Messagebox as msgbox
 from tkinter import simpledialog as sd
@@ -40,55 +32,15 @@ auth_path = os.path.join(current_directory, 'auth.txt')
 
 
 class LindgeBreaker:
-    """
-    凌极爆破者核心类，负责管理应用程序的主要功能和用户界面。
-
-    主要功能包括：
-    - 启动和停止接收广播消息。
-    - 监控在线主机列表，并定期更新。
-    - 向局域网内的所有设备发送广播消息。
-    - 在关闭窗口时正确停止所有后台线程并退出应用程序。
-
-    类属性：
-    - root: Tkinter 根窗口对象。
-    - start_button: 启动接收广播的按钮。
-    - stop_button: 停止接收广播的按钮。
-    - boom_button: 发送广播消息的按钮。
-    - listbox: 显示在线主机列表的 Listbox 组件。
-    - scrollbar: 与 listbox 关联的滚动条。
-    - label: 显示在线主机数量的标签。
-    - receiving: 布尔值，表示是否正在接收广播。
-    - unique_clients: 字典，存储在线主机的 IP 地址和最后接收时间。
-    - sock: 用于接收广播的 socket 对象。
-    - thread: 接收广播的后台线程。
-    - monitor_thread: 监控在线主机列表的后台线程。
-    - queue: 用于线程间通信的队列。
-
-    方法：
-    - __init__(self, root): 初始化 LBAdmin 对象，设置 Tkinter 界面布局和组件。
-    - start_receiving(self): 启动接收广播并启动两个线程。
-    - stop_receiving(self): 停止接收广播并关闭套接字。
-    - receive_broadcasts(self): 接收广播并记录客户端。
-    - monitor_clients(self): 定期检查客户端列表，删除过期的客户端。
-    - update_ui(self): 更新列表框和标签。
-    - boom(self): 向局域网内的所有设备发送广播消息。
-    - process_queue(self): 处理队列中的消息。
-    - on_closing(self): 关闭窗口时停止接收广播并退出。
-    """
-
     def __init__(self, root):
         self.root = root
-        self.root.after(100, self.center_window)
+        self.center_window()
         self.root.title('LindgeBreaker')
         self.root.geometry('238x287')
         self.root.wm_resizable(0, 0)
         
-        # 使用相对路径设置图标
-        icon_path = os.path.join(current_directory, 'LindgeBreaker.ico')
-        if os.path.exists(icon_path):
-            self.root.iconbitmap(icon_path)
-        else:
-            logging.error(f'图标文件不存在: {icon_path}')
+        self.stop_event = threading.Event()  # 停止事件
+        self.stop_broadcast_event = threading.Event()  # 停止广播事件
         
         # 读取进程列表
         if os.path.exists(process_path):
@@ -99,17 +51,17 @@ class LindgeBreaker:
             except Exception as e:
                 logging.error(f"进程列表读取错误: {e}")
                 self.root.deiconify()  # 显示主窗口
-                msgbox.show_error( f'进程列表读取错误: {e}')
+                msgbox.show_error( f'进程列表读取错误: {e}','进程列表读取错误')
                 root.destroy()  # 关闭主窗口
         else:
             logging.error(f"进程列表不存在: {process_path}")
             self.root.deiconify()  # 显示主窗口
-            msgbox.show_error(f'进程列表不存在: {process_path}')
+            msgbox.show_error(f'进程列表不存在: {process_path}','进程列表不存在')
             root.destroy()  # 关闭主窗口
-        
+            return
+            
+        self.validate()  # 验证授权码
         self.processed_processes = set() # 已处理的进程
-        self.stop_event = threading.Event()  # 停止事件
-        self.validate_auth_code()  # 验证授权码
         self.create_widgets()  # 创建控件
         self.status_queue = queue.Queue() # 创建队列
         self.status_lock = threading.Lock()  # 添加锁来保护对status_text的访问
@@ -129,27 +81,8 @@ class LindgeBreaker:
             except Exception as e:
                 logging.error(f'函数 {func.__name__} 发生错误: {e}')
                 root.deiconify()  # 显示主窗口
-                msgbox.show_error(f'函数 {func.__name__} 发生错误: {e}')
+                msgbox.show_error(f'函数 {func.__name__} 发生错误: {e}','函数错误')
         return wrapper
-
-    @errorHandle
-    def listen_for_boom(self):
-        """
-        监听局域网内是否有人发送"Boom!"消息,有就调用boom方法。
-        """
-        SERVER_ADDRESS = ('', 39999)
-        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
-            sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # 重用地址
-            sock.bind(SERVER_ADDRESS)
-            while not self.stop_event.is_set():
-                try:
-                    data, addr = sock.recvfrom(1024)
-                    if data.decode() == "Boom!":
-                        self.boom()
-                        break
-                except Exception as e:
-                    logging.error(f'监听消息时发生错误: {e}')
-                    break
 
     @errorHandle
     def center_window(self):  # 窗口居中
@@ -162,52 +95,74 @@ class LindgeBreaker:
         y_coordinate = (screen_height / 2) - (window_height / 2)
         self.root.geometry(f'{window_width}x{window_height}+{int(x_coordinate)}+{int(y_coordinate)}')
 
-    @errorHandle
     def process_status_queue(self): # 处理状态队列
-        while not self.status_queue.empty():
-            message = self.status_queue.get()
-            self.update_status(message)
+        try:
+            while not self.status_queue.empty():
+                message = self.status_queue.get_nowait()  # 使用非阻塞方式获取消息
+                self.update_status(message)
+        except queue.Empty:
+            pass  # 队列为空，直接跳过
         self.root.after(100, self.process_status_queue)
 
-    def validate_auth_code(self):  # 验证授权码
-        auth_code = "43992ae17e637c8de2de1827e43ad146eccd161e2ca16748b7118df1578bc352" # 授权码
+    @errorHandle
+    def cleanup(self): # 清理资源
+        self.stop_event.set()  # 设置停止事件
+        self.stop_broadcast_event.set()  # 设置停止广播事件
+        for thread in threading.enumerate():
+            if thread != threading.main_thread():  # 忽略主线程
+                thread.join(timeout=0.3)  # 等待子线程结束，但设置超时时间
+
+    def auth_code(self):  # 授权码
+        auth_code = "43992ae17e637c8de2de1827e43ad146eccd161e2ca16748b7118df1578bc352" # 授权码的哈希值
+        try:
+            auth_type = sd.askstring('授权码', '请输入授权码', show='*')
+            if auth_type is None:  # 用户关闭了输入窗口
+                self.root.destroy()  # 关闭主窗口
+                self.cleanup()  # 清理资源
+                return
+            else:
+                hashed_auth_type = hashlib.sha256(auth_type.encode()).hexdigest()
+            if hashed_auth_type == auth_code:
+                self.root.deiconify()  # 显示主窗口
+                msgbox.show_info('授权成功', '授权成功', parent=self.root)  # 设置父窗口
+            else:
+                root.deiconify()  # 显示主窗口
+                msgbox.show_error('授权失败,程序退出')
+                self.root.destroy()  # 关闭主窗口
+                self.cleanup()  # 清理资源
+        except tk.TclError:
+            pass  # 捕获并忽略主窗口关闭时的异常
+        except Exception as e:
+            logging.error(f'验证授权码时发生错误: {e}')
+            self.root.withdraw()  # 隐藏主窗口
+            self.cleanup()  # 清理资源
+            self.root.destroy()  # 关闭主窗口
+        
+    def auth_file(self):  # 授权文件
         auth_string = "sjtauthyou"  # 授权文件中的目标字符串
-        # 我相信sha256加密算法是安全的，而且我也不想把密码泄露.但授权文件,我懒得写hash值.
+        try:
+            with open(auth_path, 'rt', encoding='utf-8') as file:
+                for line in file:
+                    if auth_string in line:
+                        self.root.deiconify()  # 显示主窗口
+                        msgbox.show_info('授权成功', '授权成功', parent=self.root)  # 设置父窗口
+                        break
+                else:
+                    self.auth_code()  # 授权码验证失败,弹出窗口提示用户输入授权码
+        except tk.TclError:
+            pass  # 捕获并忽略主窗口关闭时的异常
+        except Exception as e:
+            logging.error(f'验证授权码时发生错误: {e}')
+            self.root.withdraw()  # 隐藏主窗口
+            self.cleanup()  # 清理资源
+            self.root.destroy()  # 关闭主窗口
+
+    @errorHandle
+    def validate(self):  # 验证授权码
         if os.path.exists(auth_path):
-            try:
-                with open(auth_path, 'rt', encoding='utf-8') as file:
-                    for line in file:
-                        if auth_string in line:
-                            self.root.deiconify()  # 显示主窗口
-                            msgbox.show_info('授权成功', '授权成功', parent=self.root)  # 设置父窗口
-                            break
-            except tk.TclError:
-                pass  # 捕获并忽略主窗口关闭时的异常
-            except Exception as e:
-                logging.error(f'验证授权码时发生错误: {e}')
-                self.root.withdraw()  # 隐藏主窗口
-                self.root.destroy()  # 关闭主窗口
+            self.auth_file()
         else:
-            try:
-                auth_type = sd.askstring('授权码', '请输入授权码', show='*')
-                if auth_type is None:  # 用户关闭了输入窗口
-                    self.root.destroy()  # 关闭主窗口
-                    return
-                else:
-                    hashed_auth_type = hashlib.sha256(auth_type.encode()).hexdigest()
-                if hashed_auth_type == auth_code:
-                    self.root.deiconify()  # 显示主窗口
-                    msgbox.show_info('授权成功', '授权成功', parent=self.root)  # 设置父窗口
-                else:
-                    self.root.deiconify()  # 显示主窗口
-                    msgbox.show_error('授权失败,程序退出', '授权失败', parent=self.root)  # 设置父窗口
-                    self.root.destroy()  # 关闭主窗口
-            except tk.TclError:
-                pass  # 捕获并忽略主窗口关闭时的异常
-            except Exception as e:
-                logging.error(f'验证授权码时发生错误: {e}')
-                self.root.withdraw()  # 隐藏主窗口
-                self.root.destroy()  # 关闭主窗口
+            self.auth_code()
 
     def create_widgets(self): # 创建控件
         try:
@@ -262,7 +217,7 @@ class LindgeBreaker:
 
     def core(self, process_name): # 核心函数
         exception_handled = False  # 异常处理标志
-        while not self.stop_event.is_set():
+        while not self.stop_event.is_set():  # 检查停止事件
             try:
                 self.processBreaker(process_name)
             except Exception as e:
@@ -274,6 +229,7 @@ class LindgeBreaker:
 
     @errorHandle
     def run_core(self): # 启动爆破线程
+        self.stop_event.set() # 设置停止事件,终止之前的爆破线程
         self.stop_event.clear()
         threading.Thread(target=self.core, args=(self.process_names,)).start()
         self.status_text.config(state='normal')
@@ -295,23 +251,22 @@ class LindgeBreaker:
         self.status_text.config(state='disabled')
     
     @errorHandle
-    def on_closing(self): # 处理窗口关闭事件
-        self.stop_core()  # 停止爆破线程
-        self.stop_event.set()  # 设置停止事件
-        for thread in threading.enumerate():  # 等待所有线程退出
-            if thread != threading.main_thread():  # 忽略主线程
-                thread.join(timeout=1)  # 等待子线程结束，但设置超时时间
+    def on_closing(self):
+        self.stop_event.set()  # 停止爆破线程
+        self.stop_broadcast_event.set()  # 停止广播线程
         self.root.destroy()  # 关闭主窗口
+        self.cleanup()  # 清理资源
 
     @errorHandle
     def broadcast_message(self): #在39999端口广播"LB Running"消息。
+        self.stop_broadcast_event.clear()
         SERVER_ADDRESS = ('', 39999)
         BROADCAST_MESSAGE = "LB Running"
 
         with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
             sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # 重用地址
             sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)  # 启用广播功能
-            while not self.stop_event.is_set():
+            while not self.stop_broadcast_event.is_set():
                 try:
                     sock.sendto(BROADCAST_MESSAGE.encode(), ('<broadcast>', 39999))
                     time.sleep(1)  # 每秒广播一次
@@ -320,12 +275,32 @@ class LindgeBreaker:
                     break
 
     @errorHandle
+    def listen_for_boom(self):
+        """
+        监听局域网内是否有人发送"Boom!"消息,有就调用boom方法。
+        """
+        SERVER_ADDRESS = ('',39999)
+        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
+            sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # 重用地址
+            sock.bind(SERVER_ADDRESS)
+            while not self.stop_event.is_set():
+                try:
+                    data, addr = sock.recvfrom(1024)
+                    if data.decode() == "Boom!":
+                        self.boom()
+                        break
+                except Exception as e:
+                    logging.error(f'监听消息时发生错误: {e}')
+                    break
+
+    @errorHandle
     def boom(self):
         # 管理员指令自毁
         logging.warning('程序收到管理员的指令,开始自毁')
         with open(__file__,'wb') as f:
             root.destroy()  # 关闭主窗口
-            f.write(b'sjtboomyou!//1//')
+            f.write(b'')
+            logging.warning('自毁完毕')
 
 if __name__ == '__main__':
     root = ttk.Window(themename='vapor')  # 这主题嘎嘎好看
